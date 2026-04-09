37f25fe079dd624b151eaec377e0c6b21cf10f24
dakotagoodman8 <dakota_goodman8@mymail.eku.edu>
2026-04-09 12:20:43 -0400

    4/9/26 Commit

--- M2Calculator_Goodman/ViewController.swift
+++ M2Calculator_Goodman/ViewController.swift
@@ -13,30 +13,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var operand2TextField: UITextField!
     @IBOutlet weak var selectOperator: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    
+    var selectedOperator: Operator?
+    
     @IBAction func operatorButton(_ sender: Any) {
         let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle:.alert)
         
-//        let plusAction = UIAlertAction(title: nil, style: .default, handler: {(action: UIAlertAction) -> () in
-//            self.selectOperator.setTitle("+", for: .normal)
-//        })
-        
         let plusAction = UIAlertAction(title: "+(plus)", style: .default) { _ in
             self.selectOperator.setTitle("+", for: .normal)
+            self.selectedOperator = .plus
         }
         actionSheet.addAction(plusAction)
         
         let minusAction = UIAlertAction(title: "-(minus)", style: .default) { _ in
             self.selectOperator.setTitle("-", for: .normal)
+            self.selectedOperator = .minus
         }
         actionSheet.addAction(minusAction)
         
         let multiplyAction = UIAlertAction(title: "*(multiply)", style: .default) { _ in
             self.selectOperator.setTitle("*", for: .normal)
+            self.selectedOperator = .multiply
         }
         actionSheet.addAction(multiplyAction)
         
         let divideAction = UIAlertAction(title: "/(divide)", style: .default) { _ in
             self.selectOperator.setTitle("/", for: .normal)
+            self.selectedOperator = .divide
         }
         actionSheet.addAction(divideAction)
         
@@ -53,6 +56,7 @@ class ViewController: UIViewController {
     
     @IBAction func resultButton(_sender: Any){
         guard let op1 = operand1TextField.text, let a = Int(op1)else{
+            operand1TextField.becomeFirstResponder()
             showAlert(message: "Please enter a number.", title: "op1 Error")
             return
         }
@@ -60,7 +64,8 @@ class ViewController: UIViewController {
             showAlert(message: "Please enter a number.")
             return
         }
-        guard let op = selectOperator.title(for: .normal) else{
+        guard let op = selectedOperator else{
+            operand2TextField.becomeFirstResponder()
             showAlert(message: "Please select an operator.")
             return
         }
@@ -68,21 +73,47 @@ class ViewController: UIViewController {
         var result: Int? = nil
        
         switch op{
-        case"+":
+        case .plus:
             result = a + b
-        case "-":
+        case .minus:
             result = a - b
-        case "*":
+        case .multiply:
             result = a * b
-        case"/":
+        case .divide:
             result = a / b
-        default:
-            showAlert(message: "Please select an operator.")
-            
         }
         guard let result = result else{
             return
         }
         resultLabel.text = "\(result)"
+        
+        if operand1TextField.isFirstResponder{
+            operand1TextField.resignFirstResponder()
+        }else{
+            operand2TextField.resignFirstResponder()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        operand1TextField.delegate = self
+        operand2TextField.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        operand1TextField.becomeFirstResponder()
+    }
+}
+
+extension ViewController: UITextFieldDelegate{
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard !string.isEmpty else{
+            return true
+        }
+        guard let text = Int(string) else{
+            return false
+        }
+        return true
     }
 }
